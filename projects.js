c307cac2f62d91192ae7a47e6372a6f62445c3ca
Beyonce Ebo <manduulya@yahoo.com>
2020-02-17 02:04:10 -0500

    about me text field added and toggle menu added

---
+++ projects.js
@@ -0,0 +1,17 @@
+$('.hamburger-wrapper').click(function(){
+    $('.icon').toggleClass('close');
+    });
+$('.hamburger-wrapper').mouseenter(function(){
+    $(this).animate({borderRadius: '30px'}, 0300)
+})
+$('.hamburger-wrapper').mouseleave(function(){
+    $(this).animate({borderRadius: '15px'}, 0300)
+})
+
+$(document).ready(function(){
+    $('.hamburger-wrapper').click(function(){
+        // $('.toggle').toggleClass('active')
+        $('.overlay').toggleClass('active')
+        $('.menu').toggleClass('active')
+    })
+});
