e1041563b41e3ff8eafe21110bdd92f2ba2b2f81
ebo <manduuyla@yahoo.com>
2021-10-24 01:08:02 -0400

    renewing

--- projects.js
+++ projects.js
@@ -1,16 +1,22 @@
-$('.hamburger-wrapper').click(function(){
+$('.hamburger-wrapper').click(function () {
     $('.icon').toggleClass('close');
-    });
-$('.hamburger-wrapper').mouseenter(function(){
-    $(this).animate({borderRadius: '30px'}, 0300)
+});
+$('.hamburger-wrapper').mouseenter(function () {
+    $(this).animate({ borderRadius: '30px' }, 0300)
 })
-$('.hamburger-wrapper').mouseleave(function(){
-    $(this).animate({borderRadius: '15px'}, 0300)
+$('.hamburger-wrapper').mouseleave(function () {
+    $(this).animate({ borderRadius: '15px' }, 0300)
 })
 
-$(document).ready(function(){
-    $('.hamburger-wrapper').click(function(){
+$(document).ready(function () {
+    $('.hamburger-wrapper').click(function () {
         $('.overlay').toggleClass('active')
         $('.menu').toggleClass('active')
     })
 });
+
+$('.btn-title').click(() => {
+    $('.navbar').toggleClass('hidden');
+    $('.main-content').toggleClass('hidden');
+    $('.about-me').toggleClass('hidden');
+})
